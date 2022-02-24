f52eb35eff4e7d852955e9891980d7a38c64cbe8
Carlos Alex <64806708+carlosads1998@users.noreply.github.com>
2022-02-24 19:11:14 -0300

    Update urls.py

--- virt/urls.py
+++ virt/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
+#    path('token/', TokenObtainPairView.as_view()),
+#    path('token/refresh/', TokenRefreshView.as_view()),
     path('api-auth/', include('rest_framework.urls')),
 ]
